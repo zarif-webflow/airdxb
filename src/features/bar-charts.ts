4dce13d535dd8317763b1f0d00250fc08d4ea9b7
zarif-webflow <zarif.webflow@gmail.com>
2024-11-02 01:38:07 +0600

    add cms fields to barcharts, take multiple search addresses into account

--- src/features/bar-charts.ts
+++ src/features/bar-charts.ts
@@ -2,38 +2,47 @@ import { barChart } from '@/charts/bar-chart';
 import { BarChartData } from '@/types/bar-chart';
 import { assertValue } from '@/utils/util';
 
-const volumeBarData: BarChartData = [
-  { label: 'Jumeirah Village Circle', value: 1594 },
-  { label: 'Zaabeel First', value: 1054 },
-  { label: 'Business Bay', value: 796 },
-  { label: 'Dubai Marina', value: 546 },
-  { label: 'Dubai Hills Estates', value: 437 },
-];
+const getCmsBarValues = ({ type }: { type: 'value' | 'volume' }) => {
+  const selector = type === 'value' ? '[data-value-bar-parent]' : '[data-volume-bar-parent]';
 
-const valueBarData: BarChartData = [
-  {
-    label: 'Zabeel First',
-    value: 3250,
-  },
-  {
-    label: 'Dubai Marina',
-    value: 1718,
-  },
-  {
-    label: 'Business Bay',
-    value: 1445,
-  },
-  {
-    label: 'Jumeirah Village Circle',
-    value: 1416,
-  },
-  {
-    label: 'Palm Deira',
-    value: 1339,
-  },
-];
+  const barParent = assertValue(
+    document.querySelector<HTMLElement>(selector),
+    `${selector} was not found!`
+  );
+  const cmsItems = Array.from(barParent.querySelectorAll<HTMLElement>('[data-cms-item]'));
+
+  if (cmsItems.length === 0) {
+    throw new Error(`${selector} [data-cms-item] were not found!`);
+  }
+
+  const data: BarChartData = [];
+
+  for (const cmsItem of cmsItems) {
+    const label = cmsItem.querySelector<HTMLElement>('[data-label]')?.textContent;
+    const valueStr = cmsItem.querySelector<HTMLElement>('[data-value]')?.textContent;
+    const unit = cmsItem.querySelector<HTMLElement>('[data-unit]')?.textContent || undefined;
+
+    if (!label || !valueStr) continue;
+
+    const value = Number.parseFloat(valueStr);
+
+    if (Number.isNaN(value)) continue;
+
+    data.push({ label, value, unit });
+  }
+
+  barParent.remove();
+
+  return data;
+};
 
 const initBarCharts = () => {
+  const valueBarData = getCmsBarValues({ type: 'value' });
+  const volumeBarData = getCmsBarValues({ type: 'volume' });
+
+  const valueBarUnit = valueBarData[0]?.unit;
+  const volumeBarUnit = volumeBarData[0]?.unit;
+
   const volumeBarElement = assertValue(
     document.querySelector<HTMLCanvasElement>('[data-volume-barchart]'),
     'Canvas element([data-volume-barchart]) was not found!'
@@ -64,13 +73,13 @@ const initBarCharts = () => {
       }
       return ctx.toString();
     },
-    tooltipLabelCallback: (data) => {
-      if (data.raw === 0) return '0.0';
-      if (typeof data.raw === 'number') {
-        return `${(data.raw / 1000).toFixed(1)}K`;
-      }
-      return data.toString();
-    },
+    // tooltipLabelCallback: (data) => {
+    //   if (data.raw === 0) return '0.0';
+    //   if (typeof data.raw === 'number') {
+    //     return `${(data.raw / 1000).toFixed(1)}K`;
+    //   }
+    //   return data.toString();
+    // },
     onChartInit: (chart) => {
       barTabVolumeTrigger?.addEventListener('click', () => {
         if (barTabVolumeTrigger.classList.contains('w--current')) return;
@@ -87,14 +96,14 @@ const initBarCharts = () => {
     xTickCallback: (ctx) => {
       if (ctx === 0) return '0.0';
       if (typeof ctx === 'number') {
-        return `${ctx.toLocaleString()}M`;
+        return `${ctx.toLocaleString()}${valueBarUnit}`;
       }
       return ctx.toString();
     },
     tooltipLabelCallback: (data) => {
       if (data.raw === 0) return '0.0';
       if (typeof data.raw === 'number') {
-        return `${data.raw.toLocaleString()}M`;
+        return `${data.raw.toLocaleString()}${valueBarUnit}`;
       }
       return data.toString();
     },
--- src/features/search-address.ts
+++ src/features/search-address.ts
@@ -7,278 +7,291 @@ const initSearchAddress = () => {
   /*
    * Addressing primary elements
    */
-  const addressInput = assertValue(
-    document.querySelector<HTMLInputElement>('[data-address-input]'),
-    `Address input element([data-address-input]) was not found!`
-  );
-  const addressResultContainer = assertValue(
-    addressInput
-      .closest('[data-address-container]')
-      ?.querySelector<HTMLDivElement>('[data-address-result]'),
-    `Address result container([data-address-result]) was not found!`
-  );
-  const addressResultList = assertValue(
-    addressResultContainer?.querySelector<HTMLDivElement>('[data-address-list]'),
-    `Address result list([data-address-list]) was not found!`
-  );
-  const addressResultItem = assertValue(
-    addressResultList?.querySelector<HTMLLIElement>('[data-address-item]'),
-    `Address result item([data-address-item]) was not found!`
+  const addressInputs = Array.from(
+    document.querySelectorAll<HTMLInputElement>('[data-address-input]')
   );
 
-  /*
-   * UI States
-   */
-  const modalFragment = document.createDocumentFragment();
-  let resultItems: HTMLLIElement[] = [];
-  let highlightedIndex = 0;
-  let position: 'top' | 'bottom' | undefined = undefined;
-  let isModalOpen = false;
-  const optionListId = addressResultList.id || 'address-result-options';
+  if (addressInputs.length === 0) {
+    throw new Error(`Address input element([data-address-input]) was not found!`);
+  }
 
-  let cleanupAutoUpdate: (() => void) | undefined = undefined;
-  let cleanupOutsideInteraction: (() => void) | undefined = undefined;
-
-  modalFragment.appendChild(addressResultContainer);
-  addressResultContainer.dataset.initialized = '';
-
-  /*
-   * State changer callbacks
-   */
-
-  const setupInitialAttributes = () => {
-    addressInput.type = 'search';
-    addressInput.ariaAutoComplete = 'both';
-    addressInput.setAttribute('autocomplete', 'off');
-    addressInput.setAttribute('autocorrect', 'off');
-    addressInput.setAttribute('autocapitalize', 'off');
-    addressInput.setAttribute('spellcheck', 'false');
-    addressInput.setAttribute('aria-controls', optionListId);
-
-    addressResultList.role = 'listbox';
-    addressResultList.id = optionListId;
-  };
-
-  const setModalPosition = () => {
-    computePosition(addressInput, addressResultContainer, {
-      placement: 'bottom-start',
-      middleware: [
-        offset(),
-        flip(),
-        shift(),
-        size({
-          apply: ({ rects }) => {
-            setStyle(addressResultContainer, { width: `${rects.reference.width}px` });
-          },
-        }),
-      ],
-    }).then(({ x, y }) => {
-      setStyle(addressResultContainer, { left: `${x}px`, top: `${y}px` });
-    });
-  };
-
-  const setResultItems = (items: HTMLLIElement[]) => {
-    resultItems = items;
-  };
-
-  const setHighlightedIndex = (index: number) => {
-    const prevItem = resultItems[highlightedIndex];
-    const currItem = resultItems[index];
-
-    if (prevItem) {
-      prevItem.classList.remove('focused');
-      prevItem.ariaSelected = 'false';
-    }
-
-    if (currItem) {
-      currItem.classList.add('focused');
-      currItem.ariaSelected = 'true';
-      addressInput.setAttribute('aria-activedescendant', currItem.id);
-    }
-
-    highlightedIndex = index;
-  };
-
-  let keyboardNavigationCallback: ((e: KeyboardEvent) => void) | undefined = undefined;
-
-  const selectResultItem = (index: number) => {
-    const selectedItem = resultItems[index];
-
-    const textElement = assertValue(
-      selectedItem?.querySelector<HTMLParagraphElement>('p'),
-      'List item paragraph element was not found!'
+  for (const addressInput of addressInputs) {
+    const addressResultContainer = assertValue(
+      addressInput
+        .closest('[data-address-container]')
+        ?.querySelector<HTMLDivElement>('[data-address-result]'),
+      `Address result container([data-address-result]) was not found!`
+    );
+    const addressResultList = assertValue(
+      addressResultContainer?.querySelector<HTMLDivElement>('[data-address-list]'),
+      `Address result list([data-address-list]) was not found!`
+    );
+    const addressResultItem = assertValue(
+      addressResultList?.querySelector<HTMLLIElement>('[data-address-item]'),
+      `Address result item([data-address-item]) was not found!`
     );
 
-    const text = textElement.textContent?.trim();
+    /*
+     * UI States
+     */
+    const modalFragment = document.createDocumentFragment();
+    let resultItems: HTMLLIElement[] = [];
+    let highlightedIndex = 0;
+    let position: 'top' | 'bottom' | undefined = undefined;
+    let isModalOpen = false;
+    const optionListId = addressResultList.id || 'address-result-options';
 
-    if (!text) throw new Error('Result item is invalid'!);
+    let cleanupAutoUpdate: (() => void) | undefined = undefined;
+    let cleanupOutsideInteraction: (() => void) | undefined = undefined;
 
-    addressInput.value = text;
-    addressInput.focus();
-  };
-
-  const closeResultModal = () => {
     modalFragment.appendChild(addressResultContainer);
-    addressInput.removeAttribute('aria-activedescendant');
-
-    if (keyboardNavigationCallback !== undefined) {
-      document.removeEventListener('keydown', keyboardNavigationCallback);
-    }
-
-    isModalOpen = false;
-
-    cleanupAutoUpdate?.();
-    cleanupOutsideInteraction?.();
-  };
-
-  const setupEventListeners = () => {
-    /*
-     * Click and hover effects
-     */
-    for (let i = 0; i < resultItems.length; i++) {
-      const resultItem = resultItems[i]!;
-
-      resultItem.role = 'option';
-      resultItem.ariaSelected = 'false';
-      resultItem.id = `address-suggestion-${i}`;
-
-      resultItem.addEventListener('mouseenter', () => {
-        setHighlightedIndex(i);
-      });
-
-      resultItem.addEventListener('click', () => {
-        selectResultItem(i);
-        closeResultModal();
-      });
-    }
+    addressResultContainer.dataset.initialized = '';
 
     /*
-     * Keyboard navigation
+     * State changer callbacks
      */
 
-    if (keyboardNavigationCallback !== undefined) {
-      document.removeEventListener('keydown', keyboardNavigationCallback);
-    }
+    const setupInitialAttributes = () => {
+      addressInput.type = 'search';
+      addressInput.ariaAutoComplete = 'both';
+      addressInput.setAttribute('autocomplete', 'off');
+      addressInput.setAttribute('autocorrect', 'off');
+      addressInput.setAttribute('autocapitalize', 'off');
+      addressInput.setAttribute('spellcheck', 'false');
+      addressInput.setAttribute('aria-controls', optionListId);
 
-    keyboardNavigationCallback = (e: KeyboardEvent) => {
-      if (e.key === 'Enter' || e.key === ' ') {
-        e.preventDefault();
-        addressInput.blur();
-        selectResultItem(highlightedIndex);
-        addressInput.focus();
-        closeResultModal();
-      }
-      if (e.key === 'ArrowDown') {
-        e.preventDefault();
-        setHighlightedIndex(highlightedIndex >= resultItems.length - 1 ? 0 : highlightedIndex + 1);
-      }
-      if (e.key === 'ArrowUp') {
-        e.preventDefault();
-        setHighlightedIndex(highlightedIndex <= 0 ? resultItems.length - 1 : highlightedIndex - 1);
-      }
-
-      if (e.key === 'Escape') {
-        e.preventDefault();
-        closeResultModal();
-      }
+      addressResultList.role = 'listbox';
+      addressResultList.id = optionListId;
     };
 
-    document.addEventListener('keydown', keyboardNavigationCallback);
-  };
+    const setModalPosition = () => {
+      computePosition(addressInput, addressResultContainer, {
+        placement: 'bottom-start',
+        middleware: [
+          offset(),
+          flip(),
+          shift(),
+          size({
+            apply: ({ rects }) => {
+              setStyle(addressResultContainer, { width: `${rects.reference.width}px` });
+            },
+          }),
+        ],
+      }).then(({ x, y }) => {
+        setStyle(addressResultContainer, { left: `${x}px`, top: `${y}px` });
+      });
+    };
 
-  const openResultModal = () => {
-    document.body.appendChild(addressResultContainer);
-    setModalPosition();
-    isModalOpen = true;
+    const setResultItems = (items: HTMLLIElement[]) => {
+      resultItems = items;
+    };
 
-    cleanupAutoUpdate = autoUpdate(addressInput, addressResultContainer, setModalPosition);
-    cleanupOutsideInteraction = trackInteractOutside(addressResultContainer, {
-      onPointerDownOutside: closeResultModal,
-      onInteractOutside: closeResultModal,
-      onFocusOutside: closeResultModal,
-    });
-  };
+    const setHighlightedIndex = (index: number) => {
+      const prevItem = resultItems[highlightedIndex];
+      const currItem = resultItems[index];
 
-  const renderAddressList = (addresses: string[]) => {
-    addressResultList.innerHTML = '';
-    setHighlightedIndex(0);
+      if (prevItem) {
+        prevItem.classList.remove('focused');
+        prevItem.ariaSelected = 'false';
+      }
 
-    if (addresses.length === 0) {
-      setResultItems([]);
-      return;
-    }
+      if (currItem) {
+        currItem.classList.add('focused');
+        currItem.ariaSelected = 'true';
+        addressInput.setAttribute('aria-activedescendant', currItem.id);
+      }
 
-    const resultItems: HTMLLIElement[] = [];
-    const fragment = document.createDocumentFragment();
+      highlightedIndex = index;
+    };
 
-    for (let i = 0; i < addresses.length; i++) {
-      const listItem = addressResultItem.cloneNode(true) as HTMLLIElement;
+    let keyboardNavigationCallback: ((e: KeyboardEvent) => void) | undefined = undefined;
+
+    const selectResultItem = (index: number) => {
+      const selectedItem = resultItems[index];
 
       const textElement = assertValue(
-        listItem.querySelector<HTMLParagraphElement>('p'),
+        selectedItem?.querySelector<HTMLParagraphElement>('p'),
         'List item paragraph element was not found!'
       );
 
-      textElement.textContent = addresses[i]!;
-      textElement.dataset.index = i.toString();
+      const text = textElement.textContent?.trim();
 
-      if (i === 0) {
-        listItem.classList.add('focused');
+      if (!text) throw new Error('Result item is invalid'!);
+
+      addressInput.value = text;
+      addressInput.focus();
+    };
+
+    const closeResultModal = () => {
+      modalFragment.appendChild(addressResultContainer);
+      addressInput.removeAttribute('aria-activedescendant');
+
+      if (keyboardNavigationCallback !== undefined) {
+        document.removeEventListener('keydown', keyboardNavigationCallback);
       }
 
-      fragment.appendChild(listItem);
-      resultItems.push(listItem);
-    }
+      isModalOpen = false;
 
-    addressResultList.appendChild(fragment);
+      cleanupAutoUpdate?.();
+      cleanupOutsideInteraction?.();
+    };
 
-    setResultItems(resultItems);
-    setupEventListeners();
-  };
+    const setupEventListeners = () => {
+      /*
+       * Click and hover effects
+       */
+      for (let i = 0; i < resultItems.length; i++) {
+        const resultItem = resultItems[i]!;
 
-  /*
-   * Main search event init
-   */
+        resultItem.role = 'option';
+        resultItem.ariaSelected = 'false';
+        resultItem.id = `address-suggestion-${i}`;
 
-  fetchAddresses.then(({ fetchAddresses }) => {
-    let mostRecentInputTimestamp: number | undefined = undefined;
+        resultItem.addEventListener('mouseenter', () => {
+          setHighlightedIndex(i);
+        });
 
-    addressInput.addEventListener('input', (e) => {
-      const event = e as InputEvent;
-      const inputElement = event.target as HTMLInputElement;
+        resultItem.addEventListener('click', () => {
+          selectResultItem(i);
+          closeResultModal();
+        });
+      }
 
-      const value = inputElement.value;
+      /*
+       * Keyboard navigation
+       */
 
-      const currentTimestamp = Date.now();
-      mostRecentInputTimestamp = currentTimestamp;
+      if (keyboardNavigationCallback !== undefined) {
+        document.removeEventListener('keydown', keyboardNavigationCallback);
+      }
 
-      if (value === '') {
-        renderAddressList([]);
-        closeResultModal();
+      keyboardNavigationCallback = (e: KeyboardEvent) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          addressInput.blur();
+          selectResultItem(highlightedIndex);
+          addressInput.focus();
+          closeResultModal();
+        }
+        if (e.key === 'ArrowDown') {
+          e.preventDefault();
+          setHighlightedIndex(
+            highlightedIndex >= resultItems.length - 1 ? 0 : highlightedIndex + 1
+          );
+        }
+        if (e.key === 'ArrowUp') {
+          e.preventDefault();
+          setHighlightedIndex(
+            highlightedIndex <= 0 ? resultItems.length - 1 : highlightedIndex - 1
+          );
+        }
+
+        if (e.key === 'Escape') {
+          e.preventDefault();
+          closeResultModal();
+        }
+      };
+
+      document.addEventListener('keydown', keyboardNavigationCallback);
+    };
+
+    const openResultModal = () => {
+      document.body.appendChild(addressResultContainer);
+      setModalPosition();
+      isModalOpen = true;
+
+      cleanupAutoUpdate = autoUpdate(addressInput, addressResultContainer, setModalPosition);
+      cleanupOutsideInteraction = trackInteractOutside(addressResultContainer, {
+        onPointerDownOutside: closeResultModal,
+        onInteractOutside: closeResultModal,
+        onFocusOutside: closeResultModal,
+      });
+    };
+
+    const renderAddressList = (addresses: string[]) => {
+      addressResultList.innerHTML = '';
+      setHighlightedIndex(0);
+
+      if (addresses.length === 0) {
+        setResultItems([]);
         return;
       }
 
-      fetchAddresses(value, (result: string[]) => {
-        if (mostRecentInputTimestamp !== undefined && mostRecentInputTimestamp !== currentTimestamp)
-          return;
-        renderAddressList(result);
+      const resultItems: HTMLLIElement[] = [];
+      const fragment = document.createDocumentFragment();
 
-        if (result.length === 0) {
+      for (let i = 0; i < addresses.length; i++) {
+        const listItem = addressResultItem.cloneNode(true) as HTMLLIElement;
+
+        const textElement = assertValue(
+          listItem.querySelector<HTMLParagraphElement>('p'),
+          'List item paragraph element was not found!'
+        );
+
+        textElement.textContent = addresses[i]!;
+        textElement.dataset.index = i.toString();
+
+        if (i === 0) {
+          listItem.classList.add('focused');
+        }
+
+        fragment.appendChild(listItem);
+        resultItems.push(listItem);
+      }
+
+      addressResultList.appendChild(fragment);
+
+      setResultItems(resultItems);
+      setupEventListeners();
+    };
+
+    /*
+     * Main search event init
+     */
+
+    fetchAddresses.then(({ fetchAddresses }) => {
+      let mostRecentInputTimestamp: number | undefined = undefined;
+
+      addressInput.addEventListener('input', (e) => {
+        const event = e as InputEvent;
+        const inputElement = event.target as HTMLInputElement;
+
+        const value = inputElement.value;
+
+        const currentTimestamp = Date.now();
+        mostRecentInputTimestamp = currentTimestamp;
+
+        if (value === '') {
+          renderAddressList([]);
           closeResultModal();
           return;
         }
 
-        openResultModal();
+        fetchAddresses(value, (result: string[]) => {
+          if (
+            mostRecentInputTimestamp !== undefined &&
+            mostRecentInputTimestamp !== currentTimestamp
+          )
+            return;
+          renderAddressList(result);
+
+          if (result.length === 0) {
+            closeResultModal();
+            return;
+          }
+
+          openResultModal();
+        });
       });
     });
-  });
 
-  /*
-   * Initial render
-   */
+    /*
+     * Initial render
+     */
 
-  setupInitialAttributes();
-  renderAddressList([]);
+    setupInitialAttributes();
+    renderAddressList([]);
+  }
 };
 
 initSearchAddress();
